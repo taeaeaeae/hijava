950448cb938add7afdd1bb102cb4ab617a115f74
joe753 <joe753@naver.com>
2019-01-13 13:14:27 +0900

    g

--- src/hijava/practice/n.java
+++ src/hijava/practice/n.java
@@ -5,18 +5,18 @@ public class n {
 		int a = 33;
 		int b = 22;
 		
-//		public void swap2() {
-//		int c = 1;
-//		c = a;
-//		a = b;
-//		b = c;
-//		}77
-		
 		public void swap() {
-			a = a+b;
-		    b = a-b;
-		    a = a-b;
-	}
+		int c = 1;
+		c = a;
+		a = b;
+		b = c;
+		}
+		
+//		public void swap() {
+//			a = a+b;
+//		    b = a-b;
+//		    a = a-b;
+//	}
 		public static void main(String[] args) {
 			n N = new n();
 			System.out.println("a=" + N.a);
